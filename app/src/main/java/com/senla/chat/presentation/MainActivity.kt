bd7d41cd5e3fb356747f6c642e673ccf4c4326fb
DESKTOP-ED3EJG1\serga <siarhei_yartsau@senla.eu>
2021-12-15 11:34:37 +0300

    Initial commit

---
+++ app/src/main/java/com/senla/chat/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.senla.chat.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.senla.chat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
