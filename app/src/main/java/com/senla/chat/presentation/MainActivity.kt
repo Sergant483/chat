9f1d0575613128605a420a02d2e63385156ee458
aliaksei_shynkarou <aliaksei_shynkarou@senla.eu>
2021-12-23 16:41:18 +0300

    feat: add Progress Dialog and Custom Dialog

--- app/src/main/java/com/senla/chat/presentation/MainActivity.kt
+++ app/src/main/java/com/senla/chat/presentation/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.senla.chat.presentation
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.senla.chat.R
+import com.senla.chat.presentation.fragments.utils.LoadingCustomDialog
+import com.senla.chat.presentation.fragments.utils.LoadingProgressDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loadingCustomDialog: LoadingCustomDialog
+    private lateinit var loadingProgressDialog: LoadingProgressDialog
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loadingProgressDialog()
+        loadingCustomDialog()
+
+
+
     }
+
+
+    private fun loadingCustomDialog(){
+        loadingCustomDialog = LoadingCustomDialog(this)
+        loadingCustomDialog.startLoading()
+    }
+
+    private fun loadingProgressDialog(){
+        loadingProgressDialog = LoadingProgressDialog(this)
+        loadingProgressDialog.startLoading()
+    }
+
 }
